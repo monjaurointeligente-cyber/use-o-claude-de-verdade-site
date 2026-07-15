30497b2e6b22862fdb6776fb89bf477239b2fb2a
monjaurointeligente-cyber <monjaurointeligente@gmail.com>
2026-07-15 11:27:28 -0300

    feat: add pricing page route

---
+++ src/pages/pricing.jsx
@@ -0,0 +1,13 @@
+import PricingPromptPalm from '../components/PricingPromptPalm';
+
+export default function PricingPage() {
+  return (
+    <>
+      <head>
+        <title>Pricing - PromptPalm</title>
+        <meta name="description" content="Planos de assinatura PromptPalm" />
+      </head>
+      <PricingPromptPalm />
+    </>
+  );
+}
